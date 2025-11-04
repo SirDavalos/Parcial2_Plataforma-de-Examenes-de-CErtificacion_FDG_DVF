1ca9e15ecf407941071c8eba499a478ddf938411
DiegoVenreis <diegovene1603@gmail.com>
2025-11-04 00:06:45 -0600

    ya casi2

---
+++ Frontend/js/contact.js
@@ -0,0 +1,7 @@
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const nombre = document.getElementById("nom").value;
+    const apellidoP = document.getElementById("apPat").value;
+    const apellidoM = document.getElementById("apMat").value;
+    
+});
