4cdaa7896c061a770e738632a7756b4459cd92cb
DiegoVenreis <diegovene1603@gmail.com>
2025-11-04 10:38:28 -0600

    Xd

--- Frontend/js/verificar.js
+++ Frontend/js/verificar.js
@@ -2,5 +2,22 @@ const botonR = document.getElementById("realizar");
 const botonP = document.getElementById("pagar");
 
 botonR.addEventListener("click", async() => {
+    try {
+    const res = await fetch("http://localhost:3000/api/logout", {
+      method: "POST",
+      headers: {
+        "Authorization": `Bearer ${localStorage.getItem('token')}`
+      }
+    });
     
+    if (res.ok) {
+      alert('Sesión cerrada correctamente');
+    } else {
+      const data = await res.json();
+      alert(data?.error ?? `Error al cerrar sesión`);
+    }
+    }catch (err) {
+        console.error("Error al conectar con el servidor:", err);
+        alert("Error de conexión");
+  }
 });
